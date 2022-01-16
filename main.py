55dfcd9ebedc62f0f8e206ea23b0f3c45d45975f
gliz <73669084+glixzzy@users.noreply.github.com>
2022-01-16 12:22:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ game_pin = int(input('Enter Game Pin: '))
 async def flood_game():
     client = Quickz()
     
-    while True: # a while true is done because the maximum amount of players that can join a game is 4 (while true will override that), because of the poor servers quickz has. (literally made by a child Lol with no experience.)
+    while True: # a while true is done because the maximum amount of players that can join a game is 4 (while true will override that), because of the poor servers quickz has. 
         await client.flood(game_pin)
 
 
